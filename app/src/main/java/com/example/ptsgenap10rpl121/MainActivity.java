96356594b2ec3f0bcae66f2ba65c3f89c097112b
MRaehanR <raehanrobban2@gmail.com>
2021-04-01 17:36:06 +0700

    feat: add recycler view

--- app/src/main/java/com/example/ptsgenap10rpl121/MainActivity.java
+++ app/src/main/java/com/example/ptsgenap10rpl121/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.ptsgenap10rpl121;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if(email.equalsIgnoreCase("raehan") && password.equalsIgnoreCase("123")){
                     Toast.makeText(MainActivity.this, "Anda Berhasil Login", Toast.LENGTH_SHORT).show();
+                    Intent moveDashboard = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(moveDashboard);
                 }else{
                     Toast.makeText(MainActivity.this, "Anda gagal login", Toast.LENGTH_SHORT).show();
                 }
